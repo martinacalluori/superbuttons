295bdad20949ec7cc33cc6654df5da31a303fc3f
Martina Calluori <mpc94@drexel.edu>
2018-02-13 16:56:52 -0500

    button

--- superbuttons/ViewController.swift
+++ superbuttons/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var toggleFlag: Bool = true;
+    
+    @IBOutlet weak var hemorrhoids: UIButton!
+    
+    @IBAction func hemorrhoidsFun(_ sender: Any) {
+        
+        if (toggleFlag == true) {
+            hemorrhoids.tintColor = UIColor.init(named: "sadness")
+            hemorrhoids.setBackgroundImage(UIImage(named: "people"), for: .normal)
+            toggleFlag = false
+        } else {
+            hemorrhoids.tintColor = UIColor.init(named: "fish")
+            hemorrhoids.setBackgroundImage(UIImage(named: "man_woman"), for: .normal)
+            toggleFlag = true
+        }
+        
+        let myAlert = UIAlertController(title: "Floor Preference", message: "My living room floor is?", preferredStyle: .alert)
+        
+        myAlert.addAction(UIAlertAction(title: "carpet", style: .default, handler: nil))
+        myAlert.addAction(UIAlertAction(title: "wood", style: .default, handler: nil))
+        
+        self.present(myAlert, animated:true)
+        
+      
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
